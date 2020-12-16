a6b7e0e0abead606c5300f6bbd0f8e221babe4f0
ChewCup <72440370+ChewCup@users.noreply.github.com>
2020-12-16 19:05:09 +0100

    Add project files.

---
+++ CharacterPage/app.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var fs = require("fs");
+var url = require("url");
+////////////////////////////////////////////
+
+function mainPage(res) {
+    fs.readFile("mainpage.html", function (err, html) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(html);
+        return res.end();
+    });
+}
+
+////////////////////////////////////////////
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    var p = url.parse(req.url, true);
+    if (req.url == "/") {
+        mainPage(res);
+    }
+    //res.end();
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+}).listen(8080); 
