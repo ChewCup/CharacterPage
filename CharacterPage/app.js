a76c098c2b97f7ea42575c61206435007aa91f8e
ChewCup <72440370+ChewCup@users.noreply.github.com>
2020-12-17 21:37:54 +0100

    8h work

--- CharacterPage/app.js
+++ CharacterPage/app.js
@@ -4,16 +4,49 @@ var url = require("url");
 function mainPage(res) {
     fs.readFile("mainpage.html", function (err, html) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
+        //res.write("#print { position: absolute; right: 250; top: 100;");
+        res.write("<table>")
+        let data = fs.readFileSync('charslist.lis');
+        let lines = data.toString().split(/\r?\n/);
+        for (l of lines) {
+            //res.write("<tr>\n");
+            let wordlist = l.split(',');
+            for (words of wordlist) {
+                res.write("<td>" + words + "</td>");
+            }
+            res.write("<tr>");
+        }
+        res.write("</table>");
         res.write(html);
         return res.end();
     });
 }
+function appendpage(res, query) {
+    fs.readFile("appendpage.html", function (err, html, query) {
+        res.write(html);
+        return res.end();
+    });
+        res.write("<table>");
+        var name = query.name;
+        var nameclass = query.nameclass;
+        res.write("<p>Created " + name + " as " + nameclass + " \class\ </p>");
+        fs.appendFileSync('charslist.lis', name + " " + nameclass + "\n");
+        let data = fs.readFileSync('charslist.lis');
+        let lines = data.toString().split(/\r?\n/);
+        for (l of lines) {
+            res.write("<p>" + l + "</p>");
+        }
+        res.write("</table>");
+}
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    var p = url.parse(req.url, true);
-    if (req.url == "/") {
+    var q = url.parse(req.url, true);
+    var urlpath = q.pathname;
+    if (urlpath == "/") {
         mainPage(res);
     }
-    //res.end();
+    if (urlpath == "/append") {
+        appendpage(res, q.query);
+    }
+    console.log("Serving " + req.url);
 }).listen(8080);
